33618eef7ed98d9c2e6815e1a3c84e71e2022d65
ankitaahuja <ankitaahuja111@gmail.com>
2018-10-11 20:42:10 +0000

    Feature: User Authentication Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -16,8 +15,17 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :create, :new]
+    
   end
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
